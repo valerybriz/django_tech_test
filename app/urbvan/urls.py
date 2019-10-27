9fe688fd11baaa316073c5841b9cd04bf14e583a
valery briz <valerybriz@gmail.com>
2019-10-27 14:15:14 -0600

    add the stations urls

--- app/urbvan/urls.py
+++ app/urbvan/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.urls import (include, path)
 
 from rest_framework.authtoken import views
-
 from apps.stations.urls import urlpatterns_v1_locations
+from apps.stations.urls import urls_v1_stations
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
-
-    path('v1/locations/', include(urlpatterns_v1_locations))
+    path('v1/locations/', include(urlpatterns_v1_locations)),
+    path('v1/', include(urls_v1_stations))
 ]
