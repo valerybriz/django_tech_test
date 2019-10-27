a85fab756ce3ffeb6a88edca3e002c8a341bc791
valery briz <valerybriz@gmail.com>
2019-10-27 14:14:01 -0600

    add the admin and super user factories

--- app/apps/users/factories.py
+++ app/apps/users/factories.py
@@ -18,6 +18,32 @@ class UserFactory(factory.django.DjangoModelFactory):
     is_active = True
 
 
+class UserAdminFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_staff = True
+    is_active = True
+
+
+class UserSuperUserFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_superuser = True
+    is_active = True
+
+
 class TokenFactory(factory.django.DjangoModelFactory):
 
     class Meta:
