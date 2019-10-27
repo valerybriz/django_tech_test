77e7672d609e69ed3e6bace0ac203f447c639001
valery briz <valerybriz@gmail.com>
2019-10-27 14:11:34 -0600

    initialize the stations urls

--- app/apps/stations/urls.py
+++ app/apps/stations/urls.py
@@ -1,6 +1,5 @@
 # coding: utf8
 from django.urls import path
-
 from .v1 import views as views_v1
 
 urlpatterns_v1_locations = ([
@@ -10,3 +9,26 @@ urlpatterns_v1_locations = ([
          name='v1_list_create_location'),
 
 ], 'locations')
+
+urls_v1_stations = ([
+    path(
+        'stations/',
+        views_v1.StationModelListView.as_view(),
+        name="stations-list"
+    ),
+    path(
+        'stations/<str:pk>/',
+        views_v1.StationModelDetailView.as_view(),
+        name="stations-detail"
+    ),
+    path(
+        'stations/create',
+        views_v1.StationModelCreateView.as_view(),
+        name="stations-create"
+    ),
+    path(
+        'stations/delete/<str:pk>',
+        views_v1.StationModelDeleteView.as_view(),
+        name="stations-delete"
+    ),
+], 'stations')
